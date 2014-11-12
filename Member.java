99c4dd857f0ad7fb0b52971ad79f283fec1d09f1
zoraofwater <jkim24@mail.umw.edu>
2014-11-11 19:57:39 -0500

    Member file

--- Member.java
+++ Member.java
@@ -1,3 +1,13 @@
 public class Member extends Nonmember
 {
+   Member(String name, String pass, String mail)
+   {
+      username = name;
+      password = pass;
+      email = mail;
+      member = true;
+   }
+   
+   public String username, password, email;
+   boolean member;
 }
