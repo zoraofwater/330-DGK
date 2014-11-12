b860d7b10e4c6377b710060996007a1c6f36015a
Tensei0 <mgaughan@mail.umw.edu>
2014-11-11 20:14:17 -0500

    Member 11 8:13
    More parameters

--- Member.java
+++ Member.java
@@ -1,16 +1,18 @@
 public class Member extends Nonmember
 {
-   Member(String name, String pass, String mail)
+   Member(String name, String pass, String mail, char gen, int ag)
    {
+      gender = gen;
       username = name;
       password = pass;
       email = mail;
-      win = loss = 0;
+      win = loss = challenges = 0;
       member = true;
+      age = ag;
    }
    
    public String username, password, email;
    int win, loss, challenges, age;
    boolean member;
-   char gender
+   char gender;
 }
