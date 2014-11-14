23497cd71ff7535027585c53f36ca84566079513
zoraofwater <jkim24@mail.umw.edu>
2014-11-13 20:03:38 -0500

    ListofMembers
    Something's not going right with the queue from what I thought. I'll
    work on it more when I care

--- HootHoot.java
+++ HootHoot.java
@@ -1,5 +1,7 @@
 import java.util.Scanner;
 import java.util.*;
+import java.util.Queue;
+import java.util.LinkedList;
 
 public class HootHoot {
    public static Scanner inLine = new Scanner(System.in), inChar = new Scanner(System.in), inInt = new Scanner(System.in);
@@ -9,6 +11,7 @@ public class HootHoot {
       char ch;
       int age;
       Member user;
+      Queue<ListofMembers> list;
       
       System.out.println("Type in a username: ");
       name = inLine.nextLine();
@@ -23,5 +26,8 @@ public class HootHoot {
       
       user = new Member(name,pass, email, ch, age);
       user.printInfo();
+      
+      list.add(name);
+      list.pull(name);
    }
 }
---
+++ ListofMembers.java
@@ -0,0 +1,9 @@
+public class ListofMembers
+{
+   ListofMembers(String name)
+   {
+      username = name;
+   }
+   
+   public String username;
+}
