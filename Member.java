3704742413c38fd14413ea919859f8d6f4d84f80
AshleyDudek <adudek@mail.umw.edu>
2014-11-23 22:26:43 -0500

    Added battle stuff and subscription ArrayList

--- Member.java
+++ Member.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Member extends Nonmember
 {
    Member(String name, String pass, String mail, char gen, int ag)
@@ -9,6 +11,7 @@ public class Member extends Nonmember
       win = loss = challenges = 0;
       member = true;
       age = ag;
+      subscribedTo = new ArrayList();
    }
    
    public void printInfo()
@@ -56,8 +59,55 @@ public class Member extends Nonmember
       return gender;
    }
    
+   //Battle setters and adders
+   public void setWin(int number){
+      int moreBattles = number - win;
+      win = number;
+      challenges = challenges + moreBattles;
+   }
+   
+   public void setLoss(int number){
+      int moreBattles = number - loss;
+      loss = number;
+      challenges = challenges + moreBattles;
+   }
+   
+   public void addWins(){
+      System.out.println("How many more battles have you won since you last logged in?");
+      Scanner in = new Scanner(System.in);
+      int value = in.nextInt();
+      while(value<0){
+         System.out.println("You cannot win a negative number of battles.  We should try again.");
+         System.out.println("How many more battles have you won since you last logged in?");
+         value = in.nextInt();
+      }
+      win = win + value;
+      challenges = challenges + value;
+   }
+   
+   public void addLosses(){
+      System.out.println("How many more battles have you lost since you last logged in?");
+      Scanner in = new Scanner(System.in);
+      int value = in.nextInt();
+      while(value<0){
+         System.out.println("You cannot lose a negative number of battles.  We should try again.");
+         System.out.println("How many more battles have you lost since you last logged in?");
+         value = in.nextInt();
+      }
+      loss = loss + value;
+      challenges = challenges + value;
+   }
+   
+   
+   
+   
+   
+   
    public String username, password, email;
-   int win, loss, challenges, age;
-   boolean member;
-   char gender;
+   private int win, loss, challenges, age;
+   private boolean member;
+   private char gender;
+   
+   public ArrayList<String> subscribedTo;
+   
 }
