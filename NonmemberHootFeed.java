3486076fd7e3ae8006efa96661f4983fdc69ff61
AshleyDudek <adudek@mail.umw.edu>
2014-11-24 21:16:59 -0500

    Added tagging check

--- NonmemberHootFeed.java
+++ NonmemberHootFeed.java
@@ -12,12 +12,8 @@ public class NonmemberHootFeed
 		String uName;
 		String[] tUsers = new String[10];
 		String[] hTags = new String[10];
-		
-		
-      String line = reader.readLine();
-      
-		while (line != null)
-      {
+		String line = reader.readLine();
+		while (line != null) {
 			String parts[] = line.split(" ");
 			
 			String hootText = "";
@@ -37,17 +33,14 @@ public class NonmemberHootFeed
 // parts[3] will always be an open quote
 // parts[4] will always be the first word in the hoot
 			uName = parts[1];
-			if (parts[2].compareTo("0") == 0)
-         {
+			if (parts[2].compareTo("0") == 0) {
 				priOrPub = false;
 			}
-			else 
-         {
+			else {
 				priOrPub = true;
 			}
 			
-			for (String part : parts)
-         {
+			for (String part : parts) {
 				if(i >= 0 && i <= 3){
 					
 				}
@@ -87,7 +80,109 @@ public class NonmemberHootFeed
 				System.out.println(hootText + "\n\n");
 				
 			}
-         line = reader.readLine();
+        		line = reader.readLine();
 		}
+	}
+	
+	public void amITagged(string taggedName){
+		int tagCount = 0;
+		BufferedReader reader = new BufferedReader(new FileReader("hootFile.txt"));
+		String[] tagHoots = new String[1000];
+		String[] tagUsers = new String[1000];
+		String[] tUsers = new String[10];
+		while (line != null) {
+			String parts[] = line.split(" ");
+			
+			String hootText = "";
+			String uName = "";
+			int i = 0;
+			int startHoot = 3;
+			int endHoot = 1000;
+			int endtUsers = 1000;
+			int tUserIterator = 0;
+			boolean amIinIt = false;
+			
+			uName = parts[1];
+			
+			for (String part : parts) {
+				if(i >= 0 && i <= 3){
+					
+				}
+				else if(i >= 4 && i <= endHoot){
+					if (parts[i].compareTo("ENDTHEHOOT") == 0){
+						endHoot = i;
+					}
+					else {
+						hootText = hootText + " " + parts[i];
+					}
+				}
+				else if(i > endHoot+1 && i < endtUsers){
+					if (parts[i].compareTo(")") == 0){
+						endtUsers = i;
+					}
+					else {
+						int size = parts[i].length();
+						tUsers[tUserIterator] = parts[i].substring(1, size-1);
+						tUserIterator++;
+					}
+				}
+				i++
+			}
+			
+			for(int j = 0; j < tUserIterator; j++){
+				if(taggedName.compareTo(tUsers[j]) == 0){
+					tagHoots[tagCount] = hootText;
+					tagUsers[tagCount] = uName;
+					tagCount++;
+					break;
+				}
+				else{
+					
+				}
+			}
+			
+		}
+		if(tagCount==0){
+			System.out.println("I am so sorry, it looks like nobody likes you.  So sad.");
+		}
+		else {
+			int pages = tagCount / 5;
+			int lastPageSize = tagCount % 5;
+			boolean keepGoing = true;
+			for(int pageNum = 0; pageNum < 0; pageNum++){
+				int pn = pages - pageNum;
+				System.out.println("\nPage " + pn + " of " + pages + ".");
+				for(int entryNum = 0; entryNum > 5; entryNum++){
+					System.out.print(tagUsers[tagCount - ( ( pageNum * 5 ) + 1 + entryNum)] + ": ");
+					System.out.print(tagHoots[tagCount - ( ( pageNum * 5 ) + 1 + entryNum)] + "\n\n");
+				}
+				Scanner in = new Scanner(System.in);
+				System.out.println("\nLoad another page?(y/n)");
+				String ans = "x";
+				ans = in.next();
+				ans = ans.toLowerCase();
+				while(ans.compareTo("y") != 0 && ans.compareTo("n") != 0 ){
+					System.out.println("Sorry, that is not a valid answer.");
+					System.out.println("Load another page?(y/n)");
+					ans = in.next();
+					ans = ans.toLowerCase();
+				}
+				if(ans.compareTo("n") == 0){
+					keepGoing = false;
+					break;
+				}
+				else{
+					
+				}
+			}
+			if(keepGoing == true){
+				for(int i = lastPageSize; lastPageSize > 0; lastPageSize--){
+					System.out.print(tagUsers[lastPageSize-1] + ": ");
+					System.out.print(tagHoots[lastPageSize-1] + "\n\n");
+				}
+			}
+		}
+		
+		
 	}
 }
